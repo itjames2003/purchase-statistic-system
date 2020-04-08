22cf7060640280144cd6aa57d5f68232ccd11feb
james <james@nba>
2020-04-08 20:02:06 +0800

    第一次提交

---
+++ purchase-statistic-system/src/main/java/com/infowoo/SpringBootHello.java
@@ -0,0 +1,12 @@
+package com.infowoo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+//springboot启动类
+@SpringBootApplication
+public class SpringBootHello {
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootHello.class,args);
+	}
+}
